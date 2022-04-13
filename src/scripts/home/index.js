595e596ea80993d8d808440fc5406ee3630eb398
BhryanS2 <bhryanstepenhen@gmail.com>
2022-04-12 22:11:56 -0300

    fisnish header menu

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -5,3 +5,15 @@ function toggleMenu() {
   const nav = document.querySelector(".links");
   nav.classList.toggle("active");
 }
+
+addEventListener("scroll", addClassInHeader);
+
+function addClassInHeader(event) {
+  const header = document.querySelector("header");
+  const scroll = window.scrollY;
+  if (scroll > 100) {
+    header.classList.add("headerScroll");
+  } else {
+    header.classList.remove("headerScroll");
+  }
+}
