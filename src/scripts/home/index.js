092072a8b260a6a762ec06a707ab2d71887ddacd
Ems <101734913+EmiLopes@users.noreply.github.com>
2022-04-29 12:58:41 -0300

    Delete src directory

--- src/scripts/home/index.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".bars");
-menu.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  const nav = document.querySelector(".links");
-  nav.classList.toggle("active");
-}
